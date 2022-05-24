e38e1b1936d69e9582ae802350119f444a1a3be2
RaptorIndian <raptorindian@gmail.com>
2022-05-23 21:07:30 -0400

    Added video count feature.

--- egg.py
+++ egg.py
@@ -2,6 +2,8 @@ import pyautogui as pog
 import time
 import keyboard
 
+# Sometimes does not find the save button.
+
 
 def egg():
     """
@@ -11,17 +13,17 @@ def egg():
     # Looks for the save button and clicks it.
     pog.click(pog.locateCenterOnScreen('Save.png', grayscale=True))
 
-    time.sleep(.5)
+    time.sleep(1)
 
     # Looks for the new playlist button and clicks it.
     pog.click(pog.locateCenterOnScreen('New_playlist.png', grayscale=True))
 
-    time.sleep(.5)
+    time.sleep(1)
 
     # Looks for the exit button and clicks it.
     pog.click(pog.locateCenterOnScreen('Exit.png', grayscale=True))
 
-    time.sleep(.5)
+    time.sleep(1)
 
     # These next button presses are to go to the next video.
     # Presses and holds shift.
@@ -37,11 +39,12 @@ def egg():
     # Releases shift.
     pog.keyUp('shift')
 
+    time.sleep(1.5)
 
+
+count = 1
+num = int(input("How many videos are in the playlist? "))
 # Creates a loop that will run the egg function until the the escape key is pressed.
-while True:
+while True and count <= num:
     egg()
-    # Checks if the escape key is pressed.
-    if keyboard.is_pressed('esc'):
-        print("Escape key pressed.")
-        break
+    count += 1
